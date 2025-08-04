f1ad4d09f2c47e910ffdad718466a982ae8a016c
imcrisam <Imcrisam@gmail.com>
2025-08-04 13:07:35 -0500

    feat: add Next.js configuration file with production settings

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["raw.githubusercontent.com"],
+    unoptimized: true,
+  },
+  output: 'export',
+  basePath: isProd ? '/fakewebV0' : '',
+  assetPrefix: isProd ? '/fakewebV0/' : '',
+
+  env: {
+    NEXT_PUBLIC_ASSET_PREFIX: isProd ? '/fakewebV0' : '',
+  },
+};
+
+export default nextConfig;
